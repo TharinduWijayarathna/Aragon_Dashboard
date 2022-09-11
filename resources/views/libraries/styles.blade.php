57b380227e16782818747cfebd0daaa926ca3507
Tharindu Wijayarathna <tharinduwijayarathne87@gmail.com>
2022-09-11 17:59:54 +0530

    dashboard

---
+++ resources/views/libraries/styles.blade.php
@@ -0,0 +1,16 @@
+<link rel="canonical" href="https://www.creative-tim.com/product/argon-dashboard-pro" />
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+
+<link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+<link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+
+<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+<link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+
+<link id="pagestyle" href="{{ asset('assets/css/argon-dashboard.min.css?v=2.0.5') }}" rel="stylesheet" />
+
+<style>
+    .async-hide {
+        opacity: 0 !important
+    }
+</style>
